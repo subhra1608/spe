03214b76bfd0b4558bea224392bfde4f63b1acf8
subhra1608 <Subhra.Singh@iiitb.ac.in>
2024-02-24 20:00:16 +0530

    spe_mini_project- added shaded jar plugin

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,13 +2,12 @@ package org.example;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 
 
 public class Calculator {
 
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
+
 
     public Calculator() {
     }
@@ -72,31 +71,24 @@ public class Calculator {
 
 
     public double factorial(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
         double result = fact(number1);
-        logger.info("[RESULT - FACTORIAL] - " + result);
         return result;
     }
 
 
 
     public double squareRoot(double number1) {
-        logger.info("[SQ ROOT] - " + number1);
         double result = Math.sqrt(number1);
-        logger.info("[RESULT - SQ ROOT] - " + result);
         return result;
     }
 
 
     public double power(double number1, double number2) {
-        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
         double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
         return result;
     }
 
     public double naturalLog(double number1) {
-        logger.info("[NATURAL LOG] - " + number1);
         double result = 0;
         try {
 
@@ -110,7 +102,6 @@ public class Calculator {
         } catch (ArithmeticException error) {
             System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
         }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
         return result;
     }
     public double fact(double num) {
